c6dc867f965f6395e9078931f3989775936c2726
Claude Miakouikila <miakouikilapro@gmail.com>
2023-07-25 17:40:58 +0200

    commentaire

--- asset/main.js
+++ asset/main.js
@@ -1,31 +1,34 @@
-console.log('dd')
-const buttons = document.querySelectorAll(".bloc_button button");
-console.log(buttons)
-const affichage = document.querySelector('.numero_affichage')
-const egale =  document.querySelector('.egale')
-const effacer = document.querySelector('#c')
+console.log('dd'); // Affiche "dd" dans la console lorsque la page est chargée.
 
+const buttons = document.querySelectorAll(".bloc_button button"); // Sélectionne tous les boutons dans la classe "bloc_button".
 
-buttons.forEach(buttons=>{
-    buttons.addEventListener("click",()=> {
-        const  value = buttons.textContent;
-        console.log(value)
+console.log(buttons); // Affiche les boutons sélectionnés dans la console.
 
-        affichage.innerText += value
-    })
+const affichage = document.querySelector('.numero_affichage'); // Sélectionne l'élément avec la classe "numero_affichage" qui sera utilisé pour afficher les valeurs et les résultats.
 
-})
+const egale =  document.querySelector('.egale'); // Sélectionne le bouton "=" qui sera utilisé pour effectuer le calcul.
 
-egale.addEventListener("click",()=>{
+const effacer = document.querySelector('#c'); // Sélectionne le bouton "C" qui sera utilisé pour effacer l'affichage.
+
+buttons.forEach(buttons => {
+    buttons.addEventListener("click", () => { // Ajoute un écouteur d'événement pour chaque bouton.
+        const value = buttons.textContent; // Récupère la valeur du bouton cliqué.
+        console.log(value); // Affiche la valeur du bouton dans la console.
+
+        affichage.innerText += value; // Ajoute la valeur du bouton à l'affichage.
+    });
+});
+
+egale.addEventListener("click", () => {
     try {
-        const expression = affichage.innerText;
-        const resultat = eval(expression);
-        affichage.innerText = resultat;
+        const expression = affichage.innerText; // Récupère l'expression mathématique à partir de l'affichage.
+        const resultat = eval(expression); // Évalue l'expression à l'aide de la fonction eval (à améliorer, voir commentaire précédent).
+        affichage.innerText = resultat; // Affiche le résultat dans l'affichage.
     } catch (error) {
-        affichage.innerText = "Erreur";
+        affichage.innerText = "Erreur"; // En cas d'erreur lors du calcul, affiche "Erreur" dans l'affichage.
     }
-})
+});
 
-effacer.addEventListener("click",()=>{
-    affichage.innerText = ''
-})
+effacer.addEventListener("click", () => {
+    affichage.innerText = ''; // Efface le contenu de l'affichage lorsque le bouton "C" est cliqué.
+});
